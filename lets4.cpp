0bbb9ee6720d8f39f64e67f9b4aedede3e079011
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-11-14 19:58:16 +0300

    Update lets4.cpp

--- lets4.cpp
+++ lets4.cpp
@@ -10,3 +10,11 @@ int itc_rev_bin_num(long long a1){
     }
     return a2;
 }
+int itc_mirror_count(long long a1){
+    int a2=0;
+    for (long long i=1;i<=a1;++i){
+        if (itc_mirror_num(i))
+            a2++;
+    } 
+    return a2;
+}
