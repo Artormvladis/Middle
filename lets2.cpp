23cbae0dadf345ce2e017fd6762b9d6f36030611
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-11-14 18:23:19 +0300

    Update lets2.cpp

--- lets2.cpp
+++ lets2.cpp
@@ -3,6 +3,7 @@ int itc_max_num(long long a1){
     int a2,a3,a4,a5;
     a5=0;
     a1=a1*10;
+if (a1<0){a1=-1*a1;} 
     while (a1>0){
     a2=a1%10;
     a3=a1/10%10;
@@ -22,10 +23,7 @@ int itc_max_num(long long a1){
 int itc_min_num(long long a1){
     int a2,a3,a4,a5;
     a5=0;
-    if (a1<0)
-            a1 = a1*(-1);
-    else
-        a1 = a1;
+    if (a1<0){a1=-1*a1;} 
     while (a1>0){
     a2=a1%10;
     a3=a1/10%10;
