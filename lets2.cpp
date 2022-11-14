2d2993a137790985de02f9a505037a3024b2ab3a
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-11-14 20:27:41 +0300

    Update lets2.cpp

--- lets2.cpp
+++ lets2.cpp
@@ -20,25 +20,19 @@ if (a1<0){a1=-1*a1;}
     }
     return a5;
 }
-int itc_min_num(long long a1){
-    int a2,a3,a4,a5;
-    a5=0;
-    if (a1<0){a1=-1*a1;} 
-    while (a1>0){
-    a2=a1%10;
-    a3=a1/10%10;
-    if (a2>a3){
-        a4=a3;
-    } else {
-    a4=a2;
+int itc_min_num(long long a1) {
+    int a2=10;
+    if (a1==0)
+        return 0;
+    if (a1<0)
+        a1*=-1;
+    while (a1>0) {
+        if (a1%10<a2) {
+           a2=a1%10;
+        }
+        a1/=10;
     }
-    if (a4>=a5){
-        a5=a5;
-    } else {
-    a5=a4;}
-    a1=a1/100;
-    }
-    return a5;
+    return a2;
 }
 int itc_rev_num(long long a1){
     return itc_len_num(itc_reverse_num(a1));
